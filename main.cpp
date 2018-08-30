edd380bf925144b6b9a1a71e7a87104ed3a1ec78
Kon Karashima <kon.karashima@gmail.com>
2018-08-30 22:15:44 +0900

    add Buffer Clear function

--- main.cpp
+++ main.cpp
@@ -30,6 +30,9 @@ int main(int arg, char** argc) {
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
