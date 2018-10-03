3ae5a673850371995c6b13556e7466aa7131ac9a
Kon Karashima <kon.karashima@gmail.com>
2018-10-04 00:49:48 +0900

    fix mouse scroll callback(has not enabled)

--- main.cpp
+++ main.cpp
@@ -271,6 +271,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    Camera.ProcessMouseScroll(yoffset);
 }
 
 void checkShaderError(GLuint shader) {
