832c8ca3cdc6197d377de083f61ff3f8398ae008
Kon Karashima <kon.karashima@gmail.com>
2018-10-23 23:47:31 +0900

    fix: enable ambient lighting

--- fragment_shader.fs
+++ fragment_shader.fs
@@ -17,7 +17,7 @@ void main() {
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = (diffuse) * objectColor;
+    vec3 result = (ambient + diffuse) * objectColor;
 
     FragColor = vec4(result, 1.0);
 }
