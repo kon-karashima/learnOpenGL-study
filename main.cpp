18aa9e6933ec34894fdded96efe53cbb70bce90c
Kon Karashima <kon.karashima@gmail.com>
2018-10-29 21:58:39 +0900

    add circular-moving light

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ static bool firstMouse = true;
 
 Camera Camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(1.2f, 0.5f, 2.0f);
 
 float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -140,6 +140,11 @@ int main(int arg, char** argc) {
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // change lightPos
+        float lightScale = 1.5f;
+        lightPos.x = sin(glfwGetTime()) * lightScale;
+        lightPos.z = cos(glfwGetTime()) * lightScale;
 
         // object cube shader
         cubeShader.use();
